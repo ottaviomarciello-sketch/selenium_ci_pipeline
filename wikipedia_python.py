557866d2ce636b102c431e3a0d5879a1d79a20ec
ottymar <63740443+ottymar@users.noreply.github.com>
2025-11-20 17:37:21 +0100

    modifiche
    modifiche

--- wikipedia_python.py
+++ wikipedia_python.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import StaleElementReferenceException
+from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 import time
 
@@ -23,16 +25,18 @@ def run_selenium_test():
 
     # Chrome options headless compatibili Docker/ARM
     options = webdriver.ChromeOptions()
-    options.binary_location = "/usr/bin/chromium"  # percorso di Chromium su Linux
-    options.add_argument("--headless=new")  # usa il nuovo headless mode
+    options.add_argument("--headless=new")  # usa il nuovo headless
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--log-level=3")
 
-    # Avvia Chromium con ChromeDriver
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
+    # Service per ChromeDriver (webdriver-manager gestisce il download)
+    service = Service(ChromeDriverManager().install())
+
+    # Avvia Chrome
+    driver = webdriver.Chrome(service=service, options=options)
 
     try:
         # Vai su Wikipedia
@@ -43,7 +47,7 @@ def run_selenium_test():
 
         # Campo ricerca
         search_input = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.NAME, "search"))
+            EC.visibility_of_element_located((By.ID, "searchInput"))
         )
         search_input.send_keys("Python")
 
